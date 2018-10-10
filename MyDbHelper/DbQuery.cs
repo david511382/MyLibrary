20fe086ea157b347342b93f0f11a21dc14c0d038
董欣麟 <david511382@yahoo.com.tw>
2018-10-10 18:10:05 +0800

    add project myDbHelper and unit test

---
+++ MyDbHelper/DbQuery.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDbHelper
+{
+    public static class DbQuery<T>
+    {
+        public static List<T> GetData(string sqlcmd)
+        {
+            using (SqlConnection sc = new SqlConnection(@"Data Source=(LOCALDB)\MSSQLLOCALDB;Initial Catalog=Mvc;Integrated Security=True"))
+            {
+                return sc.Query<T>(sqlcmd).ToList();
+            }
+        }
+    }
+}
---
+++ MyDbHelperTests/DbQueryTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyDbHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDbHelper.Tests
+{
+    [TestClass()]
+    public class DbQueryTests
+    {
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            List<int> ilist = DbQuery<int>.GetData("select count(1) from mvc.Members");
+            Assert.AreEqual(ilist[0], 0);
+        }
+    }
+}
