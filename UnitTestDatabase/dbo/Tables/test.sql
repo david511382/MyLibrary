f76ae91918ad401ab7c3946daa5011429274d690
david511382 <david511382@yahoo.com.tw>
2018-10-11 18:33:55 +0800

    add unit test db project

---
+++ UnitTestDatabase/dbo/Tables/test.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[test] (
+    [id]           INT          IDENTITY (1, 1) NOT NULL,
+    [varchar]      VARCHAR (10) NOT NULL,
+    [int]          INT          NOT NULL,
+    [datetime]     DATETIME     NOT NULL,
+    [varcharNull]  VARCHAR (10) NULL,
+    [intNull]      INT          NULL,
+    [datetimeNull] DATETIME     NULL
+);
+
