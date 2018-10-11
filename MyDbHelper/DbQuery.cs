7cec0e3e81869f495625a5423db8975d785b90d5
david511382 <david511382@yahoo.com.tw>
2018-10-11 18:56:42 +0800

    make exc funciton and test

--- MyDbHelper/DbQuery.cs
+++ MyDbHelper/DbQuery.cs
@@ -16,11 +16,70 @@ namespace MyDbHelper
         /// <param name="sqlcmd"></param>
         /// <param name="connectStr"></param>
         /// <returns></returns>
-        public static List<T> GetData(string sqlcmd, string connectStr)
+        public static List<T> GetData(string connectStr, string sqlcmd)
         {
             using (SqlConnection sc = new SqlConnection(connectStr))
             {
                 return sc.Query<T>(sqlcmd).ToList();
+            }
+        }
+
+        /// <summary>
+        /// async get data from connectStr by sqlcmd
+        /// </summary>
+        /// <param name="sqlcmd"></param>
+        /// <param name="connectStr"></param>
+        /// <returns></returns>
+        public static List<T> GetDataAsync(string connectStr, string sqlcmd)
+        {
+            using (SqlConnection sc = new SqlConnection(connectStr))
+            {
+                return sc.QueryAsync<T>(sqlcmd).Result.ToList();
+            }
+        }
+
+        /// <summary>
+        /// async get data from connectStr by sqlcmd
+        /// </summary>
+        /// <param name="connectStr"></param>
+        /// <param name="sqlcmd"></param>
+        /// <param name="paramss"></param>
+        /// <returns></returns>
+        public static List<T> GetDataAsync(string connectStr, string sqlcmd, KeyValuePair<string, dynamic>[] paramss)
+        {
+            DynamicParameters dynamicParameters = new DynamicParameters();
+            foreach (KeyValuePair<string, dynamic> d in paramss)
+                dynamicParameters.Add(d.Key, d.Value);
+
+            using (SqlConnection sc = new SqlConnection(connectStr))
+            {
+                return sc.QueryAsync<T>(sqlcmd, dynamicParameters).Result.ToList();
+            }
+        }
+    }
+
+    public static class DbQuery
+    {
+        /// <summary>
+        /// async exc connectStr by sqlcmd
+        /// </summary>
+        /// <param name="connectStr"></param>
+        /// <param name="sqlcmd"></param>
+        /// <param name="paramss"></param>
+        /// <returns></returns>
+        public static void ExcAsync(string connectStr, string sqlcmd, KeyValuePair<string, dynamic>[] paramss = null)
+        {
+            DynamicParameters dynamicParameters = new DynamicParameters();
+            if (paramss != null)
+                foreach (KeyValuePair<string, dynamic> d in paramss)
+                    dynamicParameters.Add(d.Key, d.Value);
+
+            using (SqlConnection sc = new SqlConnection(connectStr))
+            {
+                if (paramss != null)
+                    sc.Execute(sqlcmd, dynamicParameters);
+                else
+                    sc.Execute(sqlcmd);
             }
         }
     }
--- MyDbHelperTests/DbQueryTests.cs
+++ MyDbHelperTests/DbQueryTests.cs
@@ -14,9 +14,49 @@ namespace MyDbHelper.Tests
         [TestMethod()]
         public void GetDataTest()
         {
-            string connectStr = @"Data Source=(LOCALDB)\MSSQLLOCALDB;Initial Catalog=Mvc;Integrated Security=True";
-            List<int> ilist = DbQuery<int>.GetData("select count(1) from mvc.Members", connectStr);
+            checkCount(0);
+
+            string connectStr = @"Data Source=(LOCALDB)\MSSQLLOCALDB;Initial Catalog=UnitTest;Integrated Security=True";
+            string cmdStr = "select count(1) from dbo.test";
+            List<int> ilist = DbQuery<int>.GetDataAsync(connectStr, cmdStr);
             Assert.AreEqual(ilist[0], 0);
+        }
+
+        [TestMethod()]
+        public void GetDataParamsTest()
+        {
+            string connectStr = @"Data Source=(LOCALDB)\MSSQLLOCALDB;Initial Catalog=UnitTest;Integrated Security=True";
+            string cmdStr = "select count(1) from dbo.test Where id > @id";
+            List<int> ilist = DbQuery<int>.GetDataAsync(connectStr, cmdStr, new KeyValuePair<string, dynamic>[] { new KeyValuePair<string, dynamic>("id", 0) });
+            Assert.AreEqual(ilist[0], 0);
+        }
+
+        [TestMethod()]
+        public void ExcTest()
+        {
+            string connectStr = @"Data Source=(LOCALDB)\MSSQLLOCALDB;Initial Catalog=UnitTest;Integrated Security=True";
+            string cmdStr = "INSERT INTO [dbo].[test] ([varchar] ,[int] ,[datetime]) VALUES (@varchar,@int,@datetime)";
+            DbQuery.ExcAsync(connectStr, cmdStr, new KeyValuePair<string, dynamic>[]
+            {
+                new KeyValuePair<string, dynamic>("varchar", ""),
+                new KeyValuePair<string, dynamic>("int", 0),
+                new KeyValuePair<string, dynamic>("datetime", "2018-01-01")
+            });
+
+            checkCount(1);
+
+            cmdStr = "delete dbo.test";
+            DbQuery.ExcAsync(connectStr, cmdStr);
+
+            checkCount(0);
+        }
+
+        private void checkCount(int c)
+        {
+            string connectStr = @"Data Source=(LOCALDB)\MSSQLLOCALDB;Initial Catalog=UnitTest;Integrated Security=True";
+            string cmdStr = "select count(1) from dbo.test";
+            List<int> ilist = DbQuery<int>.GetData(connectStr, cmdStr);
+            Assert.AreEqual(ilist[0], c);
         }
     }
 }
