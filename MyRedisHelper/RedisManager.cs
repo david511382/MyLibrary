3564cc0e0cdeebcd92db0e53c3a6ae9105acb337
david511382 <david511382@yahoo.com.tw>
2018-10-12 20:35:54 +0800

    nuget to 1.0.6 myredishelper and test

--- MyRedisHelper/RedisManager.cs
+++ MyRedisHelper/RedisManager.cs
@@ -9,27 +9,35 @@ namespace MyRedisHelper
 {
     public class RedisManager
     {
-        PooledRedisClientManager _prcm;
+        private PooledRedisClientManager _prcm;
 
         public RedisManager(string redisPath)
         {
-            _prcm = new PooledRedisClientManager(new string[] { redisPath }, new string[] { redisPath }, new RedisClientManagerConfig
-            {
-                MaxWritePoolSize = 1, // “写”链接池链接数 
-                MaxReadPoolSize = 1, // “读”链接池链接数 
-                AutoStart = true,
-            });
+            _prcm = new PooledRedisClientManager(new string[] { redisPath });
+        }
 
-            ChangeDb(1);
+        public RedisManager(string readPath, string writePath)
+        {
+            _prcm = new PooledRedisClientManager(
+                new string[] { readPath },
+                new string[] { writePath },
+                new RedisClientManagerConfig
+                {
+                    MaxWritePoolSize = 1, // “写”链接池链接数 
+                    MaxReadPoolSize = 1, // “读”链接池链接数 
+                    AutoStart = true
+                });
         }
 
         public void ChangeDb(int db)
         {
-            IRedisClient Redis = _prcm.GetClient();
-            RedisClient redisClient = (RedisClient)Redis;
             try
             {
-                redisClient.ChangeDb(db);
+                using (IRedisClient Redis = _prcm.GetClient())
+                {
+                    RedisClient redisClient = (RedisClient)Redis;
+                    redisClient.ChangeDb(db);
+                }
             }
             catch (Exception e)
             {
@@ -39,32 +47,54 @@ namespace MyRedisHelper
 
         public void Set<T>(string key, T value)
         {
-            IRedisClient Redis = _prcm.GetClient();
-            RedisClient redisClient = (RedisClient)Redis;
-            redisClient.ChangeDb(1);
-            //放入内存
-            Redis.Set<T>(key, value);
+            using (IRedisClient Redis = _prcm.GetClient())
+            {
+                Redis.Set<T>(key, value);
+            }
         }
 
         public T Get<T>(string key)
         {
-            IRedisClient Redis = _prcm.GetClient();
-            return Redis.Get<T>(key);
+
+            using (IRedisClient Redis = _prcm.GetClient())
+            {
+                return Redis.Get<T>(key);
+            }
         }
 
         public string GetFromHash(string hashId, string key)
         {
-            IRedisClient Redis = _prcm.GetClient();
-            return Redis.GetValueFromHash(hashId, key);
+            using (IRedisClient Redis = _prcm.GetClient())
+            {
+                return Redis.GetValueFromHash(hashId, key);
+            }
+        }
+
+        public List<string> GetKeysFromHash(string hashId)
+        {
+            using (IRedisClient Redis = _prcm.GetClient())
+            {
+                return Redis.GetHashKeys(hashId);
+            }
+        }
+
+        public bool Remove(string key)
+        {
+            using (IRedisClient Redis = _prcm.GetClient())
+            {
+                return Redis.Remove(key);
+            }
         }
 
         public void Save()
         {
-            IRedisClient Redis = _prcm.GetClient();
-            //保存到硬盘
-            Redis.SaveAsync();
-            //释放内存
-            Redis.Dispose();
+            using (IRedisClient Redis = _prcm.GetClient())
+            {
+                //保存到硬盘
+                Redis.SaveAsync();
+                //释放内存
+                Redis.Dispose();
+            }
         }
 
     }
---
+++ MyRedisHelperTests/RedisManagerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyRedisHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRedisHelper.Tests
+{
+    [TestClass()]
+    public class RedisManagerTests
+    {
+        [TestMethod()]
+        public void ChangeDbTest()
+        {
+            string connectStr = "127.0.0.1:6379";
+            try
+            {
+                const int value = 123;
+                const string key = "test";
+                RedisManager redisManager = new RedisManager(connectStr);
+                redisManager.Set<int>(key, value);
+                int result = redisManager.Get<int>(key);
+                Assert.AreEqual(value, result);
+            }
+            catch(Exception e)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
